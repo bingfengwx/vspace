e8233cbdbacb2c4e35e3e95a6687687ff3b907e6
vanson <bingfengwx@qq.com>
2019-12-19 23:08:30 +0800

    [fix](路由)增加无空间名时的跳转
    # feat|fix|docs|style|refactor|test|chore
    
    # <body>
    
    # <footer>
    # Issue|BREAKING CHANGE
    
    # type 字段包含:
    # feat：新功能（feature）
    # fix：修补bug
    # docs：文档（documentation）
    # style： 格式（不影响代码运行的变动）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试
    # chore：构建过程或辅助工具的变动
    # scope用于说明 commit 影响的范围，比如数据层、控制层、视图层等等。
    # subject是 commit 目的的简短描述，不超过50个字符
    # Body 部分是对本次 commit 的详细描述，可以分成多行
    # Footer用来关闭 Issue或以BREAKING CHANGE开头，后面是对变动的描述、
    #       以及变动理由和迁移方法
    
    # 英文版详细例子，请看这里
    # https://github.com/sparkbox/how_to/tree/master/style/git
    # 中文版介绍，请看这里
    # http://www.ruanyifeng.com/blog/2016/01/commit_message_change_log.html

--- vspace-ui/src/router/index.js
+++ vspace-ui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Space from '../views/Space.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,13 @@ const routes = [
     component: Home
   },
   {
+    path: '/space',
+    redirect: '/'
+  },
+  {
     path: '/space/:spaceName',
     name: 'space',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "space" */ '../views/Space.vue'),
+    component: Space,
     props: true
   }
 ]
